fb348e75eade30e105fbdf7fd11e91b59c719155
Joey Bakx <joey.bakx@incentro.com>
2018-12-22 22:48:02 +0100

    loaded week stats based on mockdata

--- src/app/components/weeklyStats/ProgressTable.jsx
+++ src/app/components/weeklyStats/ProgressTable.jsx
@@ -2,7 +2,55 @@ import React, { Component } from "react";
 import { Table, Icon } from "semantic-ui-react";
 import "./ProgressTable.css";
 
+//TODO: Set an API response instead of mock data
+let weeklyStat = [
+  {
+    weekNumber: 1,
+    averageWeight: 60.2,
+    calorieGoal: 2100,
+    caloriesConsumed: 1849,
+    expectedWeightRange: "60.4 - 60.8"
+  },
+  {
+    weekNumber: 2,
+    averageWeight: 60.6,
+    calorieGoal: 2000,
+    caloriesConsumed: 2049,
+    expectedWeightRange: "60.4 - 60.8"
+  }
+];
+
+const userMockData = {
+  id: 12319232103021,
+  firstName: "Joey",
+  lastName: "Bakx",
+  username: "djowie",
+  metric: "kg",
+  profilePicture: "https://randomuser.me/api/portraits/med/men/65.jpg",
+  lastWeightIn: {
+    weight: 60.2,
+    fatpercentage: 12.9,
+    date: "2018-09-22T20:13:30+00:00"
+  },
+  averageStats: {
+    weight: 60.2,
+    fatpercentage: 12.9,
+    lastUpdated: "2018-09-22T20:13:30+00:00"
+  },
+  goal: "bulk",
+  level: "intermediate",
+  minDelta: 0.1133980925,
+  maxDelta: 0.226796185,
+  nutritionRatio: {
+    lastModifiedDate: "2018-09-22T22:12:06+00:00",
+    protein: 30,
+    carbs: 45,
+    fats: 25
+  }
+};
 export default class ProgressTable extends Component {
+  state = { weeklyStat: weeklyStat, user: userMockData };
+
   render() {
     return (
       <div>
@@ -14,33 +62,38 @@ export default class ProgressTable extends Component {
               <Table.HeaderCell>Average Weight</Table.HeaderCell>
               <Table.HeaderCell>Calorie Goal</Table.HeaderCell>
               <Table.HeaderCell>Average Calorie Realization</Table.HeaderCell>
-              <Table.HeaderCell>Weekly Delta</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
 
           <Table.Body>
-            <Table.Row>
-              <Table.Cell disabled>1</Table.Cell>
-              <Table.Cell>60.2</Table.Cell>
-              <Table.Cell disabled>1900</Table.Cell>
-              <Table.Cell negative>1849</Table.Cell>
-              <Table.Cell disabled>
-                <Icon name="minus circle" />
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell disabled>2</Table.Cell>
-              <Table.Cell>
-                60.6 <span className="weight-ranges">60.4 - 60.8</span>
-              </Table.Cell>
-              <Table.Cell disabled>1900</Table.Cell>
-              <Table.Cell positive>1949</Table.Cell>
-              <Table.Cell disabled positive>
-                <Icon name="arrow alternate circle up" />
-                {/* <Icon name="arrow alternate circle down " /> */}
-                0.4
-              </Table.Cell>
-            </Table.Row>
+            {this.state.weeklyStat.map(week => (
+              <Table.Row>
+                <Table.Cell disabled>{week.weekNumber}</Table.Cell>
+                <Table.Cell>
+                  60.6{" "}
+                  <span className="weight-ranges">
+                    {week.expectedWeightRange}
+                  </span>
+                </Table.Cell>
+                <Table.Cell disabled>{week.calorieGoal}</Table.Cell>
+                {this.state.user.goal === "bulk" &&
+                  (week.calorieGoal > week.caloriesConsumed && (
+                    <Table.Cell negative>{week.caloriesConsumed}</Table.Cell>
+                  ))}
+                {this.state.user.goal === "bulk" &&
+                  (week.calorieGoal < week.caloriesConsumed && (
+                    <Table.Cell positive>{week.caloriesConsumed}</Table.Cell>
+                  ))}
+                {this.state.user.goal === "cut" &&
+                  (week.calorieGoal > week.caloriesConsumed && (
+                    <Table.Cell positive>{week.caloriesConsumed}</Table.Cell>
+                  ))}
+                {this.state.user.goal === "cut" &&
+                  (week.calorieGoal < week.caloriesConsumed && (
+                    <Table.Cell negative>{week.caloriesConsumed}</Table.Cell>
+                  ))}
+              </Table.Row>
+            ))}
           </Table.Body>
         </Table>
         Last sync: xx-xx-xx xx/xx/xx
